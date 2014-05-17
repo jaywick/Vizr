7689f7cdfef90dd403c54b42451531c7ae481505
Jay Wick <developer@jay-wick.com>
2014-05-17 14:52:09 +1000

    Refactored Commands class

--- Vizr/Commands.cs
+++ Vizr/Commands.cs
@@ -14,6 +14,8 @@ namespace Vizr
         private CommandsList commands;
         private string path;
 
+        #region Serialization
+
         public Commands()
         {
             path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "commands.xml");
@@ -65,67 +67,12 @@ namespace Vizr
             }
         }
 
+        #endregion
+
         public IEnumerable<Command> Query(string text)
         {
             var results = commands.AllItems.Where(c => c.Name.ToLower().StartsWith(text.ToLower().Trim()) || c.Name.Contains(" " + text.ToLower().Trim()));
             return results;
-        }
-
-        [XmlRoot("Items")]
-        public class CommandsList
-        {
-            private string filePath;
-
-            public CommandsList()
-            {
-                Items = new List<Command>();
-                MetaItems = new List<Command>();
-                addMetaCommands();
-            }
-
-            public CommandsList(string filePath)
-                : this()
-            {
-                this.filePath = filePath;
-            }
-
-            private void addMetaCommands()
-            {
-                // about app
-                MetaItems.Add(new Command()
-                {
-                    Name = "About",
-                    Subtitle = "About this app",
-                    CommandName = "explorer.exe",
-                    Arguments = "https://github.com/jaywick/vizr",
-                    HitCount = 1
-                });
-
-                // edit commands
-                MetaItems.Add(new Command()
-                {
-                    Name = "Edit",
-                    Subtitle = "Edit commands",
-                    CommandName = "explorer.exe",
-                    Arguments = filePath,
-                    HitCount = 1
-                });
-            }
-
-            [XmlElement("Command")]
-            public List<Command> Items { get; set; }
-
-            [XmlIgnore]
-            public List<Command> MetaItems { get; set; }
-
-            [XmlIgnore]
-            public List<Command> AllItems
-            {
-                get
-                {
-                    return Items.Union(MetaItems).ToList();
-                }
-            }
         }
     }
 }
---
+++ Vizr/CommandsList.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Vizr
+{
+    [XmlRoot("Items")]
+    public class CommandsList
+    {
+        private string filePath;
+
+        public CommandsList()
+        {
+            Items = new List<Command>();
+            MetaItems = new List<Command>();
+            addMetaCommands();
+        }
+
+        public CommandsList(string filePath)
+            : this()
+        {
+            this.filePath = filePath;
+        }
+
+        private void addMetaCommands()
+        {
+            // about app
+            MetaItems.Add(new Command()
+            {
+                Name = "About",
+                Subtitle = "About this app",
+                CommandName = "explorer.exe",
+                Arguments = "https://github.com/jaywick/vizr",
+                HitCount = 1
+            });
+
+            // edit commands
+            MetaItems.Add(new Command()
+            {
+                Name = "Edit",
+                Subtitle = "Edit commands",
+                CommandName = "explorer.exe",
+                Arguments = filePath,
+                HitCount = 1
+            });
+        }
+
+        [XmlElement("Command")]
+        public List<Command> Items { get; set; }
+
+        [XmlIgnore]
+        public List<Command> MetaItems { get; set; }
+
+        [XmlIgnore]
+        public List<Command> AllItems
+        {
+            get
+            {
+                return Items.Union(MetaItems).ToList();
+            }
+        }
+    }
+}
