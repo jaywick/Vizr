2a370d22b864b94601d6008f151f03feca3059a8
Jay Wick <developer@jay-wick.com>
2014-05-17 12:10:40 +1000

    Initial UI skeleton

---
+++ Vizr/Extensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+
+namespace Vizr
+{
+    static class Extensions
+    {
+        public static void SelectNext(this ListBox target)
+        {
+            if (target.SelectedIndex == -1)
+                return; // nothing selected
+
+            if (target.SelectedIndex == target.Items.Count - 1)
+                target.SelectedIndex = 0; // loop back to top
+            else
+                target.SelectedIndex++; // go to next
+        }
+
+        public static void SelectPrevious(this ListBox target)
+        {
+            if (target.SelectedIndex == -1)
+                return; // nothing selected
+
+            if (target.SelectedIndex == 0)
+                target.SelectedIndex = target.Items.Count - 1; // loop back to top
+            else
+                target.SelectedIndex--; // go to next
+        }
+    }
+}
---
+++ Vizr/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Vizr;
+
+namespace Vizr
+{
+    public partial class MainWindow : Window
+    {
+        private bool processTextChange = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            textQuery.Text = "";
+
+            processTextChange = true;
+            textQuery.Focus();
+        }
+
+        private void Window_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Escape:
+                    this.Close();
+                    break;
+                case Key.Up:
+                    listResults.SelectPrevious();
+                    break;
+                case Key.Down:
+                    listResults.SelectNext();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void textQuery_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!processTextChange) return;
+
+            var message = textQuery.Text;
+
+            listResults.Items.Clear();
+            if (message == "d")
+            { 
+                listResults.Items.Add("hai");
+                listResults.Items.Add("2");
+            }
+
+            if (listResults.Items.Count != 0)
+                listResults.SelectedIndex = 0;
+        }
+    }
+}
